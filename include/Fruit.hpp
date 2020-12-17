fccb458aa1bc6fdb7aabb952f9a23e257e4b6ff0
Mateusz Chrusciel <tiskus.3009@gmail.com>
2020-12-17 17:12:21 +0100

    Fruit now doesn;t spawn on edges of the screen

--- include/Fruit.hpp
+++ include/Fruit.hpp
@@ -4,16 +4,19 @@ class Fruit
 {
 public:
     cv::Point fruitPoint;
-    int fruitRadius = 20;
+    const int fruitRadius = 20;
+    const int windowOffset = 25 + fruitRadius;
 
-    void generatePoint()
+    void generatePoint(const cv::Point &screenSize)
     {
-        fruitPoint = cv::Point(rand() % 590 + 25, rand() % 430 + 25);
+        fruitPoint = cv::Point(rand() % (screenSize.x - 2*windowOffset) + windowOffset, rand() % (screenSize.y - 2*windowOffset) + windowOffset);
     }
 
-    bool checkIfEat(const cv::Point &addedPoint)
+    bool checkIfEat(/* const cv::Point &previousPoint, */ const cv::Point &addedPoint)
     {
+        //return (int)(std::abs((addedPoint.x - previousPoint.x)*(previousPoint.y - fruitPoint.y) - (previousPoint.x - fruitPoint.x)*(addedPoint.y-previousPoint.y))/sqrt((addedPoint.x-previousPoint.x)*(addedPoint.x-previousPoint.x) + (addedPoint.y-previousPoint.y)*(addedPoint.y-previousPoint.y))) < fruitRadius;
         return sqrt(pow((addedPoint.x - fruitPoint.x),2) + pow((addedPoint.y - fruitPoint.y),2)) <= fruitRadius;
         //return addedPoint.x > fruitPoint.x && addedPoint.x < fruitPoint.x + fruitRadius && addedPoint.y > fruitPoint.y && addedPoint.y < fruitPoint.y + fruitRadius;
+
     }
 };
