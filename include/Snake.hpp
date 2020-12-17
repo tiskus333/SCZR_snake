07315fc3966b13da398eb5f791aab46f67716240
Mateusz Chrusciel <tiskus.3009@gmail.com>
2020-12-17 17:12:52 +0100

    added score system

--- include/Snake.hpp
+++ include/Snake.hpp
@@ -12,16 +12,19 @@ public:
     Fruit snakeFruit;
 
     int allowedLength = 50;
+    const int ADD_LENGTH = 5;
     int length = 0;
+    int score = 0;
+    cv::Point ScreenSize;
 
-Snake()
+Snake(const cv::Point &screenSize) : ScreenSize(screenSize) 
     {
-        snakeFruit.generatePoint();
+        snakeFruit.generatePoint(ScreenSize);
     }
 
     void incAllowedLength()
     {
-        allowedLength += 5;
+        allowedLength += ADD_LENGTH;
     }
 
     bool ifIntersected(const cv::Point &a, const cv::Point &b, const cv::Point &c, const cv::Point &d)
@@ -31,7 +34,7 @@ Snake()
         checkOrient[1] = orientation(a, b, d);
         checkOrient[2] = orientation(c, d, a);
         checkOrient[3] = orientation(c, d, b);
-        return (checkOrient[0] != checkOrient[1] && checkOrient[2] != checkOrient[3]) ? true : false;
+        return (checkOrient[0] != checkOrient[1]) && (checkOrient[2] != checkOrient[3]);
     }
 
     int orientation(const cv::Point &a, const cv::Point &b, const cv::Point &c)
@@ -75,16 +78,18 @@ Snake()
 
     bool checkIfEaten()
     {
-        return snakeFruit.checkIfEat(snakeBody.back());
+        return snakeFruit.checkIfEat(/* *(snakeBody.end()-2), */ *(snakeBody.end()-1));
     }
+
     void draw(cv::Mat &frame)
     {
         if(snakeBody.size() > 0)
         for( auto it = snakeBody.rbegin(); it != snakeBody.rend() - 1; ++it)
         {
-            cv::line(frame,*it, *(it+1),{255,8,127},5);
+            cv::line(frame,*it, *(it+1),{127,8,255},5);
         }
         cv::circle(frame, snakeFruit.fruitPoint, snakeFruit.fruitRadius, {0,255,0}, cv::FILLED);
+        cv::putText(frame,"SCORE: "+std::to_string(score),{ScreenSize.x - 200, 30},cv::HersheyFonts::FONT_HERSHEY_DUPLEX,1,{255,0,0},2);
     }
     
     bool calculateSnake(const cv::Point &point)
@@ -130,8 +135,9 @@ Snake()
 
             if (checkIfEaten())
             {
+                ++score;
                 incAllowedLength();
-                snakeFruit.generatePoint();
+                snakeFruit.generatePoint(ScreenSize);
             }
         }
         return false;
