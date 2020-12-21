af6c27668873905242b84eb2f9bdd22b46ff2fb6
Konrad Bratosiewicz <konrad.bratosiewicz@gmail.com>
2020-12-21 12:16:49 +0100

    Fixed closing of process B with message queues

--- include/MessageQueue.h
+++ include/MessageQueue.h
@@ -11,17 +11,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const long MSGQ_MAX_MSG = 10;       // maximum 10
-
-const long PAYLOAD = 4096;
+#include <time.h>
 
 class MessageQueue {
   mqd_t msgq_des_;
   struct mq_attr attributes_;
+  struct timespec timeout;
 
   // constants
   const long PAYLOAD_SIZE = 4096;
-  const long MSGQ_MAX_MSG = 10;       // maximum 10
+  const long MSGQ_MAX_MSG = 1;       // maximum 10
+  time_t TIMEOUT = 10;
 
   public:
   MessageQueue();
@@ -32,11 +32,5 @@ class MessageQueue {
   void sendFrame(char *p_data, const size_t data_size);
   void receiveFrame(char *p_data, const size_t data_size);
 };
-
-void createMessageQueue(const char *msgq_file_name);
-mqd_t openMessageQueue(const char *msgq_file_name, const int flag);
-
-void msgqSendFrame(int msgq_des, char *p_data, const size_t data_size);
-void msgqReceiveFrame(int msgq_des, char *p_data, const size_t data_size);
 
 #endif
--- src/MessageQueue.cpp
+++ src/MessageQueue.cpp
@@ -3,7 +3,7 @@
 MessageQueue::MessageQueue() {
   attributes_.mq_flags = 0;
   attributes_.mq_maxmsg = MSGQ_MAX_MSG;
-  attributes_.mq_msgsize = PAYLOAD;
+  attributes_.mq_msgsize = PAYLOAD_SIZE;
   attributes_.mq_curmsgs = 0;
 }
 
@@ -30,8 +30,12 @@ void MessageQueue::close() {
 
 void MessageQueue::sendFrame(char *p_data, const size_t data_size) {
   size_t i = 0;
-  for (; i < data_size / PAYLOAD; ++i) {
-    if (mq_send(msgq_des_, p_data + i * PAYLOAD_SIZE, PAYLOAD_SIZE, 1) == -1) {
+
+  clock_gettime(CLOCK_REALTIME, &timeout);
+  timeout.tv_sec += TIMEOUT;
+
+  for (; i < data_size / PAYLOAD_SIZE; ++i) {
+    if (mq_timedsend(msgq_des_, p_data + i * PAYLOAD_SIZE, PAYLOAD_SIZE, 1, &timeout) == -1) {
       perror("mq_send");
       exit(1);
     }
@@ -41,52 +45,12 @@ void MessageQueue::sendFrame(char *p_data, const size_t data_size) {
 void MessageQueue::receiveFrame(char *p_data, const size_t data_size) {
   size_t i = 0;
   uint priority;
-  for (; i < data_size / PAYLOAD; ++i) {
-    if (mq_receive(msgq_des_, p_data + i * PAYLOAD_SIZE, PAYLOAD_SIZE, &priority) == -1) {
-      perror("mq_receive");
-      exit(1);
-    }
-  }
-}
 
-void createMessageQueue(const char *msgq_file_name) {
-  struct mq_attr frame;
-  frame.mq_flags = 0;
-  frame.mq_maxmsg = MSGQ_MAX_MSG;
-  frame.mq_msgsize = PAYLOAD;
-  frame.mq_curmsgs = 0;
-  mqd_t msg_q = mq_open(msgq_file_name, O_CREAT | O_EXCL | O_RDWR,
-                        S_IREAD | S_IWRITE, &frame);
-  if (msg_q == -1) {
-    perror("Cannot create message queue");
-    exit(EXIT_FAILURE);
-  }
-}
+  clock_gettime(CLOCK_REALTIME, &timeout);
+  timeout.tv_sec += TIMEOUT;
 
-mqd_t openMessageQueue(const char *msgq_file_name, const int flag) {
-  mqd_t msgq_des = mq_open(msgq_file_name, flag);
-  if (msgq_des == -1) {
-    perror("Cannot open message queue");
-    exit(1);
-  }
-  return msgq_des;
-}
-
-void msgqSendFrame(int msgq_des, char *p_data, const size_t data_size) {
-  size_t i = 0;
-  for (; i < data_size / PAYLOAD; ++i) {
-    if (mq_send(msgq_des, p_data + i * PAYLOAD, PAYLOAD, 1) == -1) {
-      perror("mq_send");
-      exit(1);
-    }
-  }
-}
-
-void msgqReceiveFrame(int msgq_des, char *p_data, const size_t data_size) {
-  size_t i = 0;
-  uint priority;
-  for (; i < data_size / PAYLOAD; ++i) {
-    if (mq_receive(msgq_des, p_data + i * PAYLOAD, PAYLOAD, &priority) == -1) {
+  for (; i < data_size / PAYLOAD_SIZE; ++i) {
+    if (mq_timedreceive(msgq_des_, p_data + i * PAYLOAD_SIZE, PAYLOAD_SIZE, &priority, &timeout) == -1) {
       perror("mq_receive");
       exit(1);
     }
